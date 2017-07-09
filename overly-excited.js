62b74853cc5e5dc1380691a1d7dc46c649b8f593
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-07-09 13:05:48 -0500

    committed

--- overly-excited.js
+++ overly-excited.js
@@ -1,5 +1,5 @@
 //the sentence
-var words = 'The walrus danced through the trees in the light of the moon'
+var words = ' The walrus danced through the trees in the light of the moon'
 
 // Create an array that contains the words in the sentence
 var sentence = words.split(' ');
@@ -10,7 +10,16 @@ function addExcitement (theWordArray) {
       Write a `for` loop that iterates over the array argument and
       outputs the words.
      */
+     var message = ''
+     for (var i = 1; i < theWordArray.length; i++) {
+     	if (i % 3 === 0) {
+     		message += theWordArray[i] + '! ';
+     	} else {
+			message += theWordArray[i] + ' ';
+     	}
+		console.log(message);
+     }
 }
 
 // Invoke the function and pass in the array
-addExcitement(sentence)
+addExcitement(sentence);
